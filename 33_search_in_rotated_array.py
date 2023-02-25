683653d30ef39c2c3fd6edb764518d762b3bad5a
amoghmc <amoghmc@github.com>
2023-02-25 21:59:39 +0530

    refactor 33

--- 33_search_in_rotated_array.py
+++ 33_search_in_rotated_array.py
@@ -39,24 +39,24 @@ from typing import List
 
 class Solution:
 	def search(self, nums: List[int], target: int) -> int:
-		l, r = 0, len(nums) - 1
+		left_ptr, right_ptr = 0, len(nums) - 1
 
-		while l <= r:
-			mid = (l + r) // 2
+		while left_ptr <= right_ptr:
+			mid = (left_ptr + right_ptr) // 2
 			if target == nums[mid]:
 				return mid
 			# left sorted portion
-			if nums[l] <= nums[mid]:
-				if target < nums[l] or target > nums[mid]:
-					l = mid + 1
+			if nums[left_ptr] <= nums[mid]:
+				if target < nums[left_ptr] or target > nums[mid]:
+					left_ptr = mid + 1
 				else:
-					r = mid - 1
+					right_ptr = mid - 1
 			# right sorted portion
 			else:
-				if target >= nums[l] or target < nums[mid]:
-					r = mid - 1
+				if target >= nums[left_ptr] or target < nums[mid]:
+					right_ptr = mid - 1
 				else:
-					l = mid + 1
+					left_ptr = mid + 1
 		return -1
 
 
