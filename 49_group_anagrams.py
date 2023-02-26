00f1743e6b1a754be953d1ad395c3c083584cd98
amoghmc <amoghmc@github.com>
2023-02-26 23:30:51 +0530

    add 49

--- 49_group_anagrams.py
+++ 49_group_anagrams.py
@@ -1,34 +1,29 @@
 #!/home/linuxbrew/.linuxbrew/bin/python3
 """
+Given an array of strings strs, group the anagrams together.
+ You can return the answer in any order.
 
-Given an array of integers nums and an integer target,
-return indices of the two numbers such that they add up to target.
+An Anagram is a word or phrase formed by rearranging the letters
+of a different word or phrase, typically using all the original letters exactly once.
 
-You may assume that each input would have exactly one solution,
-and you may not use the same element twice.
-
-You can return the answer in any order.
 
 Example 1:
-	Input: nums = [2,7,11,15], target = 9
-	Output: [0,1]
-	Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+	Input: strs = ["eat","tea","tan","ate","nat","bat"]
+	Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
 
 Example 2:
-	Input: nums = [3,2,4], target = 6
-	Output: [1,2]
+	Input: strs = [""]
+	Output: [[""]]
 
 Example 3:
-	Input: nums = [3,3], target = 6
-	Output: [0,1]
-
+	Input: strs = ["a"]
+	Output: [["a"]]
 
 Constraints:
-	2 <= nums.length <= 104
-	-109 <= nums[i] <= 109
-	-109 <= target <= 109
-	Only one valid answer exists.
 
+	1 <= strs.length <= 104
+	0 <= strs[i].length <= 100
+	strs[i] consists of lowercase English letters.
 https://leetcode.com/problems/two-sum/
 """
 import unittest
@@ -48,9 +43,9 @@ class Solution:
 
 		for i in range(len(nums)):
 			complement = target - nums[i]
-			index = hashmap[complement]
-			if complement in hashmap and index != i:
-				return [i, index]
+			j = hashmap[complement]
+			if complement in hashmap and j != i:
+				return [i, j]
 
 
 class TestSolution(unittest.TestCase):
